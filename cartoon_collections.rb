f26ef61f07e757d8d22f5c4d09a6c6362809cf43
Riley Moore <rilemoore@gmail.com>
2019-10-14 21:01:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves()
+def roll_call_dwarves(dwarves)
+  # Your code here
+  dwarves.each do |dwarf|
+    puts dwarf
+  end
+  
+  
+end
+
+def summon_captain_planet()
   # Your code here
 end
 
-def summon_captain_planet(# code an argument here)
+def long_planeteer_calls()
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese()
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
