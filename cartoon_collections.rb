3fba7c77eb91b18ad099bcbd39248449513f7261
Riley Moore <rilemoore@gmail.com>
2019-10-14 21:00:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves()
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(# code an argument here)
   # Your code here
 end
 
